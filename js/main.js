8ca35a18f8b38e6320662546d6beeacc8312b0d1
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-11-03 17:15:35 +0100

    localstorage

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const listaProductos = [];
 //Agregar
 formulario.addEventListener("submit", (ev) => {
     ev.preventDefault();
-    const valor = producto.value;
+    const valor = producto.value.toLowerCase().trim();
     agregarProducto(valor);
 })
 
@@ -20,21 +20,39 @@ formulario.addEventListener("submit", (ev) => {
 
 //FUNCIONES
 
+const validar = (valor) => {
+    const regexp = /^[a-záéíóúüñÁÉÍÓÚÜÑ]+$/gi;
+    //comprobar que sean letras, no números, no símbolos, + espacio entre palabras?
+    if (regexp.test(valor)){
+        console.log(true);
+        return true;
+    } else {
+        alert('Escribe el producto correctamente.');
+        console.log(false);
+        return false;
+    }
+}
 
-// const validar = (input) => {
-    
-
-//     return (true/false)
-// }
 const agregarProducto = (valor) => {
-        console.log(producto.value);
-    //recibir
-    //validar(input);
-    // to local
+    let valido = true;
+    valido = validar(valor);
+    if (valido === true) {
+
+        const productoCarro = {nombre: `${valor}`, cantidad: `${1}`};
+        listaProductos = JSON.parse(localStorage.getItem(productoCarro)) || []
+        productoCarro.forEach(() => {
+        })
+    }
+    }
+        localStorage.setItem('productoCarro', JSON.stringify(productoCarro));
+        console.log(productoCarro)
+        // to local
     //Si válido - pasamos y guardamos en localStorage {nombre, cantidad}
+//     Pintar {
+
     //Si un producto ya existe, su contador se incrementa en lugar de duplicarlo.
     //Creamos y pintamos la fila con el producto nuevo (nombre, cantidad y el botón eliminar)o incrementamos el que existe
-}
+
 
 const eliminarProducto = () => {
     //al hacer clic, debe decrementar el contador de ese producto. 
