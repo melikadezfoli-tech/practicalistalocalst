f2b176cf0b1934d462fdec8c02ea50fa3a0fcbd2
HannaPolishchuk <hanna.polishchuk@gmail.com>
2025-11-03 20:56:34 +0100

    Arreglé la función arreglarProducto y añadí las funciones de apoyo.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,11 @@
 const formulario = document.querySelector('#formulario');
 const producto = document.querySelector('#producto');
 const carrito = document.querySelector('#carrito');
-const listaProductos = [];
-// const listaProductos = ['boligrafo','lápiz','libreta','goma','sacapuntas','grapadora'];
-
+let listaProductos = [];
 
 //EVENTOS
 //Agregar
-formulario.addEventListener("submit", (ev) => {
+formulario.addEventListener('submit', (ev) => {
     ev.preventDefault();
     const valor = producto.value.toLowerCase().trim();
     agregarProducto(valor);
@@ -20,43 +18,63 @@ formulario.addEventListener("submit", (ev) => {
 
 //FUNCIONES
 
-const validar = (valor) => {
-    const regexp = /^[a-záéíóúüñÁÉÍÓÚÜÑ]+$/gi;
-    //comprobar que sean letras, no números, no símbolos, + espacio entre palabras?
-    if (regexp.test(valor)){
-        console.log(true);
+const validarInput = (valor) => {
+    //comprobar que sean letras y pueden estar separadas por un espacio, 
+    // no números, no símbolos,
+    const regexp = /^[a-záéíóúüñÁÉÍÓÚÜÑ\s]+$/gi;
+    if (regexp.test(valor)) {
         return true;
     } else {
         alert('Escribe el producto correctamente.');
-        console.log(false);
         return false;
     }
 }
 
+const comprobarRepetido = (productosLocal, valor) => {
+    //una variable para devolver un valor booleano fuera del forEach
+    let encontrado = false;
+    productosLocal.forEach((producto) => {
+        if (producto.nombre === valor) {
+            producto.cantidad++;
+            console.log("iguales")
+            console.log(producto.nombre);
+            encontrado =  true;
+        }
+    });
+    return encontrado;
+}
+
+//Creamos y pintamos la fila con el producto nuevo 
+// (nombre, cantidad y el botón eliminar) o incrementamos el que existe
+const pintarTabla = () => {
+
+}
+
+
 const agregarProducto = (valor) => {
     let valido = true;
-    valido = validar(valor);
+    valido = validarInput(valor);
     if (valido === true) {
+        //obtener productos
+        //Busca el valor en localStorage; si no existe, devuelve null.
+        //Si se devuelve null, se asigna un valor vacío [] para evitar errores.
+        productosLocal = JSON.parse(localStorage.getItem('listaProductos')) || [];
+        console.log(listaProductos);
+        //Comprobamos si el producto está repetido
+        let productoRepetido = comprobarRepetido(productosLocal, valor);
 
-        const productoCarro = {nombre: `${valor}`, cantidad: `${1}`};
-        listaProductos = JSON.parse(localStorage.getItem(productoCarro)) || []
-        productoCarro.forEach(() => {
-        })
+        if (productoRepetido === false) {
+            productosLocal.push({ nombre: valor, cantidad: 1 });
+        }
+        //guardar productos:
+        localStorage.setItem('listaProductos', JSON.stringify(productosLocal));
+        pintarTabla();
     }
-    }
-        localStorage.setItem('productoCarro', JSON.stringify(productoCarro));
-        console.log(productoCarro)
-        // to local
-    //Si válido - pasamos y guardamos en localStorage {nombre, cantidad}
-//     Pintar {
+}
 
-    //Si un producto ya existe, su contador se incrementa en lugar de duplicarlo.
-    //Creamos y pintamos la fila con el producto nuevo (nombre, cantidad y el botón eliminar)o incrementamos el que existe
 
 
 const eliminarProducto = () => {
     //al hacer clic, debe decrementar el contador de ese producto. 
-// Si el contador llega a cero, el producto debe eliminarse de la lista y del Local Storage.
+    // Si el contador llega a cero, el producto debe eliminarse de la lista y del Local Storage.
 }
-
-/**ALMACENAR EN LOCAL */
